0739f54b7ece124e741b7d2921b1bd93cac2ab34
Vlad Kozin <vladilen.kozin@gmail.com>
2019-03-14 14:00:49 +0000

    Implement ht constructor for hash-tables
    Allow parameterizing which equality to use and immutability of created
    hash-tables.

--- prelude.rkt
+++ prelude.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require syntax/parse (for-syntax syntax/parse))
 
 (define-syntax-rule (comment . any) (void))
 (define-syntax-rule (example . any) (void))
@@ -80,3 +81,95 @@
     (set! h (set: h 'c 'd 'e 42)))
   (check eq? 42 (get: h 'c 'd 'e))
   (check-pred immutable? (get: h 'c 'd)))
+
+
+(comment
+ ;; TODO #:associative struct prop
+ (struct foo (v) #:associative)
+ ;; =>
+ (struct foo (v)
+   #:methods gen:associative
+   ((define (assoc-get))
+    (define (assoc-set))))
+ ;; comment
+ )
+
+
+(define ht-equality (make-parameter equal?))
+(define ht-immutable (make-parameter false))
+
+
+(define (ht-constructor)
+
+  (define ((op-is? eq) op)
+    (eq? eq op))
+
+  (match (list (ht-equality) (ht-immutable))
+
+    ((list (? (op-is? eq?)) #t) make-immutable-hasheq)
+    ((list (? (op-is? eq?)) #f) make-hasheq)
+
+    ((list (? (op-is? eqv?)) #t) make-immutable-hasheqv)
+    ((list (? (op-is? eqv?)) #f) make-hasheqv)
+
+    ((list (? (op-is? equal?)) #t) make-immutable-hash)
+    ((list (? (op-is? equal?)) #f) make-hash)))
+
+
+(define-syntax (ht stx)
+  (syntax-parse stx
+    ;; with equality
+    ((_ #:eq op:expr (key:expr value:expr) ...)
+     #'(parameterize ((ht-equality op)) ((ht-constructor) `((,key . ,value) ...))))
+
+    ((_ (key:expr value:expr) ... #:eq op:expr)
+     #'(parameterize ((ht-equality op)) ((ht-constructor) `((,key . ,value) ...))))
+
+    ;; with default equality
+    ((_ (key:expr value:expr) ...)
+     #'((ht-constructor) `((,key . ,value) ...)))))
+
+
+(define-syntax (hti stx)
+  (syntax-parse stx
+    ;; with equality
+    ((_ #:eq op:expr (key:expr value:expr) ...)
+     #'(parameterize ((ht-equality op) (ht-immutable true)) ((ht-constructor) `((,key . ,value) ...))))
+
+    ((_ (key:expr value:expr) ... #:eq op:expr)
+     #'(parameterize ((ht-equality op) (ht-immutable true)) ((ht-constructor) `((,key . ,value) ...))))
+
+    ;; with default equality
+    ((_ (key:expr value:expr) ...)
+     #'(parameterize ((ht-immutable true)) ((ht-constructor) `((,key . ,value) ...))))))
+
+
+(module+ test
+
+  (let ((key "key"))
+    (check-eq? 42 (get: (ht (key 1) ('nested (ht ('foo 42))) #:eq eqv?) 'nested 'foo)))
+
+  (let ((h (ht ('type 1) ('tag 42))))
+    (check-false (immutable? h))
+    (check-pred hash-equal? h))
+
+  (let ((h (ht ('type 1) ('tag 42) #:eq eqv?)))
+    (check-false (immutable? h))
+    (check-pred hash-eqv? h))
+
+  (let ((h (ht #:eq eq? ('type 1) ('tag 42))))
+    (check-false (immutable? h))
+    (check-pred hash-eq? h))
+
+  (let ((h (hti #:eq eq? ('type 1) ('tag 42))))
+    (check-pred immutable? h)
+    (check-pred hash-eq? h))
+
+  (parameterize ((ht-immutable true)
+                 (ht-equality eqv?))
+    (let ((h (ht ('type 1) ('tag 42))))
+      (check-pred immutable? h)
+      (check-pred hash-eqv? h))))
+
+
+;; TODO maybe #lang prelude with better defaults
