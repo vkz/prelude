cc24b9656965dd43c9497435300c20ee28bbffdf
Vlad Kozin <vladilen.kozin@gmail.com>
2019-03-30 22:48:21 +0000

    Provide #lang prelude

---
+++ main.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require "prelude.rkt")
+(require racket/undefined)
+
+(provide (except-out (all-from-out racket) or and if when)
+         (all-from-out racket/undefined)
+         (except-out (all-from-out "prelude.rkt") or? and? if? when?)
+         (rename-out [or?   or]
+                     [and?  and]
+                     [if?   if]
+                     [when? when]))
+
+(module reader syntax/module-reader prelude)
